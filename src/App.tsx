5ba26cdbe1f353cbc463b00109d841ac9d0a86ae
!Kpose <ganihujude@gmail.com>
2022-09-02 21:47:21 +0100

    add reanimated and gesture handler

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import RootNavigator from './navigation/RootNavigator';
 import {ThemeProvider} from './providers/ThemeProvider';
 import {SpeachProvider} from './providers/SpeachProvider';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <ThemeProvider>
-        <SpeachProvider>
-          <RootNavigator />
-        </SpeachProvider>
+        <GestureHandlerRootView style={{flex: 1}}>
+          <SpeachProvider>
+            <RootNavigator />
+          </SpeachProvider>
+        </GestureHandlerRootView>
       </ThemeProvider>
     </SafeAreaProvider>
   );
