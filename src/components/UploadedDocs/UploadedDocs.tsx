3049165075c66bc3d2582d347a8ea57a1b656b89
!Kpose <ganihujude@gmail.com>
2022-09-12 01:37:05 +0100

    seperate child component

---
+++ src/components/UploadedDocs/UploadedDocs.tsx
@@ -0,0 +1,100 @@
+import {View, FlatList, StyleSheet} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import Screen from '../Screen/Screen';
+import Text from '../Text/Text';
+import {RNPdftron} from 'react-native-pdftron';
+import {useUser} from '../../providers/UserProvider';
+import useCloudStorage from '../../hooks/CloudStorage.hook';
+import storage from '@react-native-firebase/storage';
+import RNFS from 'react-native-fs';
+
+const UploadedDocs = () => {
+  const [userDocs, setUserDocs] = useState([]);
+  const {user} = useUser();
+  const {getAllDocs} = useCloudStorage();
+  const [jjj, setFileLocation] = useState('');
+
+  useEffect(() => {
+    RNPdftron.initialize('');
+    getAll();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getAll = useCallback(async () => {
+    const email = user?.email;
+    if (!email) {
+      return;
+    }
+    const data = await getAllDocs(email);
+    setUserDocs(data);
+  }, [getAllDocs, user?.email]);
+
+  const ItemSeparatorView = () => {
+    return <View style={styles.inputContainer} />;
+  };
+
+  const getAndWritePdf = useCallback(async (fullPath: any, name: string) => {
+    const url = await storage()
+      .ref(fullPath)
+      .getDownloadURL()
+      .catch(e => {
+        console.error(e);
+      });
+    // create a local file path from url
+    const localFile = `${RNFS.DocumentDirectoryPath}/${name}`;
+    const options = {
+      fromUrl: url,
+      toFile: localFile,
+    };
+    // download with fileviewer.
+    RNFS.downloadFile(options)
+      .promise.then(() => setFileLocation(localFile))
+      .then(() => {
+        // success
+      })
+      .catch(error => {
+        console.log(error);
+        // error
+      });
+  }, []);
+
+  const ItemView = ({item}) => {
+    return (
+      <View style={styles.itemView}>
+        <Text onPress={() => getAndWritePdf(item.fullPath, item.name)}>
+          File Name: {item.name}
+          {'\n'}
+          File Full Path: {item.fullPath}
+          {'\n'}
+          Bucket: {item.bucket}
+        </Text>
+      </View>
+    );
+  };
+
+  return (
+    <Screen>
+      {userDocs.length ? (
+        <FlatList
+          data={userDocs}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      ) : null}
+    </Screen>
+  );
+};
+
+export default UploadedDocs;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    height: 0.5,
+    width: '100%',
+    backgroundColor: '#C8C8C8',
+  },
+  itemView: {
+    // padding: 10,
+  },
+});
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,84 +1,21 @@
-import {View, FlatList, StyleSheet, Alert} from 'react-native';
-import React, {useCallback, useEffect, useState} from 'react';
+import {Alert} from 'react-native';
+import React, {useCallback} from 'react';
 import Screen from '../../components/Screen/Screen';
 import Text from '../../components/Text/Text';
 import FAB from '../../components/FAB/FAB';
 import {FolderIcon} from '../../components/Icon/Icon';
-import useCloudStorage from '../../hooks/CloudStorage.hook';
-import storage from '@react-native-firebase/storage';
-import RNFS from 'react-native-fs';
-import {RNPdftron} from 'react-native-pdftron';
+import UploadedDocs from '../../components/UploadedDocs/UploadedDocs';
 import {requestFilePermission} from '../../utils/Permissions.util';
 import {selectPdf} from '../../utils/FilePicker.util';
-import {useUser} from '../../providers/UserProvider';
 import DocumentPicker, {
   DocumentPickerResponse,
 } from 'react-native-document-picker';
+import {useUser} from '../../providers/UserProvider';
+import useCloudStorage from '../../hooks/CloudStorage.hook';
 
 const Home = () => {
-  const {getAllDocs} = useCloudStorage();
-  const [userDocs, setUserDocs] = useState([]);
-  const [jjj, setFileLocation] = useState('');
-  const {/* percent */ uploadFile} = useCloudStorage();
   const {user} = useUser();
-
-  const getAll = useCallback(async () => {
-    const email = user?.email;
-    if (!email) {
-      return;
-    }
-    const data = await getAllDocs(email);
-    setUserDocs(data);
-  }, [getAllDocs, user?.email]);
-
-  useEffect(() => {
-    RNPdftron.initialize('');
-    getAll();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const getAndWritePdf = useCallback(async (fullPath: any, name: string) => {
-    const url = await storage()
-      .ref(fullPath)
-      .getDownloadURL()
-      .catch(e => {
-        console.error(e);
-      });
-    // create a local file path from url
-    const localFile = `${RNFS.DocumentDirectoryPath}/${name}`;
-    const options = {
-      fromUrl: url,
-      toFile: localFile,
-    };
-    // download with fileviewer.
-    RNFS.downloadFile(options)
-      .promise.then(() => setFileLocation(localFile))
-      .then(() => {
-        // success
-      })
-      .catch(error => {
-        console.log(error);
-        // error
-      });
-  }, []);
-
-  const ItemSeparatorView = () => {
-    return <View style={styles.inputContainer} />;
-  };
-
-  const ItemView = ({item}) => {
-    return (
-      <View style={styles.itemView}>
-        <Text onPress={() => getAndWritePdf(item.fullPath, item.name)}>
-          File Name: {item.name}
-          {'\n'}
-          File Full Path: {item.fullPath}
-          {'\n'}
-          Bucket: {item.bucket}
-        </Text>
-      </View>
-    );
-  };
+  const {uploadFile} = useCloudStorage();
 
   const handleFileSelected = useCallback(
     async (selectedFile: DocumentPickerResponse) => {
@@ -118,30 +55,13 @@ const Home = () => {
     <Screen>
       <Text>Home</Text>
       <FolderIcon size={15} />
-      {userDocs.length ? (
-        <FlatList
-          data={userDocs}
-          ItemSeparatorComponent={ItemSeparatorView}
-          renderItem={ItemView}
-          keyExtractor={(item, index) => index.toString()}
-        />
-      ) : null}
-      <FAB onImportPress={onImportPress} />
 
+      <UploadedDocs />
+
+      <FAB onImportPress={onImportPress} />
       {/* {jjj && <DocumentView document={jjj} showLeadingNavButton={true} />} */}
     </Screen>
   );
 };
 
 export default Home;
-
-const styles = StyleSheet.create({
-  inputContainer: {
-    height: 0.5,
-    width: '100%',
-    backgroundColor: '#C8C8C8',
-  },
-  itemView: {
-    padding: 10,
-  },
-});
